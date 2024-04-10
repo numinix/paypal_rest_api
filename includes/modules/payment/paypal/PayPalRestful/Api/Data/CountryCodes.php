0c5f9fad39fe267d970a0606e47c92c3c3bcc62e
lat9 <lat9@vinosdefrutastropicales.com>
2024-04-10 11:05:54 -0400

    #20: Actually update those country-codes!

--- includes/modules/payment/paypal/PayPalRestful/Api/Data/CountryCodes.php
+++ includes/modules/payment/paypal/PayPalRestful/Api/Data/CountryCodes.php
@@ -5,213 +5,268 @@
  * @copyright Copyright 2023 Zen Cart Development Team
  * @license https://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: lat9 2023 Nov 16 Modified in v2.0.0 $
+ *
+ * Last updated: v1.0.2
  */
 namespace PayPalRestful\Api\Data;
 
+// -----
+// For reference, see https://developer.paypal.com/api/rest/reference/orders/v2/country-address-requirements/
+//
+// Which is a different list than https://developer.paypal.com/api/rest/reference/country-codes/ (sigh)
+//
 class CountryCodes
 {
     protected static array $countryCodes = [
-        'AL',  //- ALBANIA
-        'DZ',  //- ALGERIA
-        'AD',  //- ANDORRA
-        'AO',  //- ANGOLA
-        'AI',  //- ANGUILLA
-        'AG',  //- ANTIGUA & BARBUDA
-        'AR',  //- ARGENTINA
-        'AM',  //- ARMENIA
-        'AW',  //- ARUBA
-        'AU',  //- AUSTRALIA
-        'AT',  //- AUSTRIA
-        'AZ',  //- AZERBAIJAN
-        'BS',  //- BAHAMAS
-        'BH',  //- BAHRAIN
-        'BB',  //- BARBADOS
-        'BY',  //- BELARUS
-        'BE',  //- BELGIUM
-        'BZ',  //- BELIZE
-        'BJ',  //- BENIN
-        'BM',  //- BERMUDA
-        'BT',  //- BHUTAN
-        'BO',  //- BOLIVIA
-        'BA',  //- BOSNIA & HERZEGOVINA
-        'BW',  //- BOTSWANA
-        'BR',  //- BRAZIL
-        'VG',  //- BRITISH VIRGIN ISLANDS
-        'BN',  //- BRUNEI
-        'BG',  //- BULGARIA
-        'BF',  //- BURKINA FASO
-        'BI',  //- BURUNDI
-        'KH',  //- CAMBODIA
-        'CM',  //- CAMEROON
-        'CA',  //- CANADA
-        'CV',  //- CAPE VERDE
-        'KY',  //- CAYMAN ISLANDS
-        'TD',  //- CHAD
-        'CL',  //- CHILE
-        'C2',  //- CHINA ... Zen Cart code is 'CH'
-        'CO',  //- COLOMBIA
-        'KM',  //- COMOROS
-        'CG',  //- CONGO - BRAZZAVILLE
-//        'CD',  //- CONGO - KINSHASA ... not a Zen Cart country!
-        'CK',  //- COOK ISLANDS
-        'CR',  //- COSTA RICA
-        'CI',  //- CÔTE D’IVOIRE
-        'HR',  //- CROATIA
-        'CY',  //- CYPRUS
-        'CZ',  //- CZECH REPUBLIC
-        'DK',  //- DENMARK
-        'DJ',  //- DJIBOUTI
-        'DM',  //- DOMINICA
-        'DO',  //- DOMINICAN REPUBLIC
-        'EC',  //- ECUADOR
-        'EG',  //- EGYPT
-        'SV',  //- EL SALVADOR
-        'ER',  //- ERITREA
-        'EE',  //- ESTONIA
-        'ET',  //- ETHIOPIA
-        'FK',  //- FALKLAND ISLANDS
-        'FO',  //- FAROE ISLANDS
-        'FJ',  //- FIJI
-        'FI',  //- FINLAND
-        'FR',  //- FRANCE
-        'GF',  //- FRENCH GUIANA
-        'PF',  //- FRENCH POLYNESIA
-        'GA',  //- GABON
-        'GM',  //- GAMBIA
-        'GE',  //- GEORGIA
-        'DE',  //- GERMANY
-        'GI',  //- GIBRALTAR
-        'GR',  //- GREECE
-        'GL',  //- GREENLAND
-        'GD',  //- GRENADA
-        'GP',  //- GUADELOUPE
-        'GT',  //- GUATEMALA
-        'GN',  //- GUINEA
-        'GW',  //- GUINEA-BISSAU
-        'GY',  //- GUYANA
-        'HN',  //- HONDURAS
-        'HK',  //- HONG KONG SAR CHINA
-        'HU',  //- HUNGARY
-        'IS',  //- ICELAND
-        'IN',  //- INDIA
-        'ID',  //- INDONESIA
-        'IE',  //- IRELAND
-        'IL',  //- ISRAEL
-        'IT',  //- ITALY
-        'JM',  //- JAMAICA
-        'JP',  //- JAPAN
-        'JO',  //- JORDAN
-        'KZ',  //- KAZAKHSTAN
-        'KE',  //- KENYA
-        'KI',  //- KIRIBATI
-        'KW',  //- KUWAIT
-        'KG',  //- KYRGYZSTAN
-        'LA',  //- LAOS
-        'LV',  //- LATVIA
-        'LS',  //- LESOTHO
-        'LI',  //- LIECHTENSTEIN
-        'LT',  //- LITHUANIA
-        'LU',  //- LUXEMBOURG
-        'MK',  //- MACEDONIA
-        'MG',  //- MADAGASCAR
-        'MW',  //- MALAWI
-        'MY',  //- MALAYSIA
-        'MV',  //- MALDIVES
-        'ML',  //- MALI
-        'MT',  //- MALTA
-        'MH',  //- MARSHALL ISLANDS
-        'MQ',  //- MARTINIQUE
-        'MR',  //- MAURITANIA
-        'MU',  //- MAURITIUS
-        'YT',  //- MAYOTTE
-        'MX',  //- MEXICO
-        'FM',  //- MICRONESIA
-        'MD',  //- MOLDOVA
-        'MC',  //- MONACO
-        'MN',  //- MONGOLIA
-        'ME',  //- MONTENEGRO
-        'MS',  //- MONTSERRAT
-        'MA',  //- MOROCCO
-        'MZ',  //- MOZAMBIQUE
-        'NA',  //- NAMIBIA
-        'NR',  //- NAURU
-        'NP',  //- NEPAL
-        'NL',  //- NETHERLANDS
-        'NC',  //- NEW CALEDONIA
-        'NZ',  //- NEW ZEALAND
-        'NI',  //- NICARAGUA
-        'NE',  //- NIGER
-        'NG',  //- NIGERIA
-        'NU',  //- NIUE
-        'NF',  //- NORFOLK ISLAND
-        'NO',  //- NORWAY
-        'OM',  //- OMAN
-        'PW',  //- PALAU
-        'PA',  //- PANAMA
-        'PG',  //- PAPUA NEW GUINEA
-        'PY',  //- PARAGUAY
-        'PE',  //- PERU
-        'PH',  //- PHILIPPINES
-        'PN',  //- PITCAIRN ISLANDS
-        'PL',  //- POLAND
-        'PT',  //- PORTUGAL
-        'QA',  //- QATAR
-        'RE',  //- RÉUNION
-        'RO',  //- ROMANIA
-        'RU',  //- RUSSIA
-        'RW',  //- RWANDA
-        'WS',  //- SAMOA
-        'SM',  //- SAN MARINO
-        'ST',  //- SÃO TOMÉ & PRÍNCIPE
-        'SA',  //- SAUDI ARABIA
-        'SN',  //- SENEGAL
-        'RS',  //- SERBIA
-        'SC',  //- SEYCHELLES
-        'SL',  //- SIERRA LEONE
-        'SG',  //- SINGAPORE
-        'SK',  //- SLOVAKIA
-        'SI',  //- SLOVENIA
-        'SB',  //- SOLOMON ISLANDS
-        'SO',  //- SOMALIA
-        'ZA',  //- SOUTH AFRICA
-        'KR',  //- SOUTH KOREA
-        'ES',  //- SPAIN
-        'LK',  //- SRI LANKA
-        'SH',  //- ST. HELENA
-        'KN',  //- ST. KITTS & NEVIS
-        'LC',  //- ST. LUCIA
-        'PM',  //- ST. PIERRE & MIQUELON
-        'VC',  //- ST. VINCENT & GRENADINES
-        'SR',  //- SURINAME
-        'SJ',  //- SVALBARD & JAN MAYEN
-        'SZ',  //- SWAZILAND
-        'SE',  //- SWEDEN
-        'CH',  //- SWITZERLAND
-        'TW',  //- TAIWAN
-        'TJ',  //- TAJIKISTAN
-        'TZ',  //- TANZANIA
-        'TH',  //- THAILAND
-        'TG',  //- TOGO
-        'TO',  //- TONGA
-        'TT',  //- TRINIDAD & TOBAGO
-        'TN',  //- TUNISIA
-        'TM',  //- TURKMENISTAN
-        'TC',  //- TURKS & CAICOS ISLANDS
-        'TV',  //- TUVALU
-        'UG',  //- UGANDA
-        'UA',  //- UKRAINE
-        'AE',  //- UNITED ARAB EMIRATES
-        'GB',  //- UNITED KINGDOM
-        'US',  //- UNITED STATES
-        'UY',  //- URUGUAY
-        'VU',  //- VANUATU
-        'VA',  //- VATICAN
-        'VE',  //- VENEZUELA
-        'VN',  //- VIETNAM
-        'WF',  //- WALLIS & FUTUNA
-        'YE',  //- YEMEN
-        'ZM',  //- ZAMBIA
-        'ZW',  //- ZIMBABWE
+        'AF',  //- Afghanistan
+        'AX',  //- Aland Islands
+        'AL',  //- Albania
+        'DZ',  //- Algeria
+        'AS',  //- American Samoa
+        'AD',  //- Andorra
+        'AO',  //- Angola
+        'AI',  //- Anguilla
+        'AQ',  //- Antarctica
+        'AG',  //- Antigua and Barbuda
+        'AR',  //- Argentina
+        'AM',  //- Armenia
+        'AW',  //- Aruba
+        'AU',  //- Australia
+        'AT',  //- Austria
+        'AZ',  //- Azerbaijan
+        'BS',  //- Bahamas
+        'BH',  //- Bahrain
+        'BD',  //- Bangladesh
+        'BB',  //- Barbados
+        'BY',  //- Belarus
+        'BE',  //- Belgium
+        'BZ',  //- Belize
+        'BJ',  //- Benin
+        'BM',  //- Bermuda
+        'BT',  //- Bhutan
+        'BO',  //- Bolivia
+        'BA',  //- Bosnia and Herzegovina
+        'BW',  //- Botswana
+        'BV',  //- Bouvet Island
+        'BR',  //- Brazil
+        'IO',  //- British Indian Ocean Territory
+        'BN',  //- Brunei Darussalam
+        'BG',  //- Bulgaria
+        'BF',  //- Burkina Faso
+        'BI',  //- Burundi
+        'KH',  //- Cambodia
+        'CM',  //- Cameroon
+        'CA',  //- Canada
+        'CV',  //- Cape Verde Escudo
+        'KY',  //- Cayman Islands
+        'CF',  //- Central African Republic
+        'TD',  //- Chad
+        'CL',  //- Chile
+        'C2',  //- China ... Zen Cart code is 'CH'
+        'CN',  //- China
+        'CX',  //- Christmas Island
+        'CC',  //- Cocos Islands
+        'CO',  //- Colombia
+        'KM',  //- Comoros
+//        'CD',  //- Congo Democratic Republic, not a Zen Cart country!
+//        'ZR',  //- Congo Democratic Republic, not a Zen Cart country!
+        'CG',  //- Congo Republic
+        'CK',  //- Cook Islands
+        'CR',  //- Costa Rica
+        'CI',  //- Cote Divoire
+        'HR',  //- Croatia
+        'CU',  //- Cuba
+        'CY',  //- Cyprus
+        'CZ',  //- Czech Republic
+        'DK',  //- Denmark
+        'DJ',  //- Djibouti
+        'DM',  //- Dominica
+        'DO',  //- Dominican Republic
+//        'TP',  //- East Timor, not a Zen Cart country!
+        'EC',  //- Ecuador
+        'EG',  //- Egypt
+        'SV',  //- El Salvador
+        'GQ',  //- Equatorial Guinea
+        'ER',  //- Eritrea
+        'EE',  //- Estonia
+        'ET',  //- Ethiopia
+        'FK',  //- Falkland Islands
+        'FO',  //- Faroe Islands
+        'FM',  //- Federated States Of Micronesia
+        'FJ',  //- Fiji
+        'FI',  //- Finland
+        'FR',  //- France
+        'GF',  //- French Guiana
+        'PF',  //- French Polynesia
+        'TF',  //- French Southern Territories
+        'GA',  //- Gabon Republic
+        'GM',  //- Gambia
+        'GE',  //- Georgia Country
+        'DE',  //- Germany
+        'GH',  //- Ghana
+        'GI',  //- Gibraltar
+        'GR',  //- Greece
+        'GL',  //- Greenland
+        'GD',  //- Grenada
+        'GP',  //- Guadeloupe
+        'GU',  //- Guam
+        'GT',  //- Guatemala
+        'GG',  //- Guernsey
+        'GN',  //- Guinea
+        'GW',  //- Guinea Bissau
+        'GY',  //- Guyana
+        'HT',  //- Haiti
+        'HM',  //- Heard and Mcdonald Islands
+        'HN',  //- Honduras
+        'HK',  //- Hong Kong
+        'HU',  //- Hungary
+        'IS',  //- Iceland
+        'IN',  //- India
+        'ID',  //- Indonesia
+        'IR',  //- Iran
+        'IQ',  //- Iraq
+        'IE',  //- Ireland
+        'IM',  //- Isle of Man
+        'IL',  //- Israel
+        'IT',  //- Italy
+        'JM',  //- Jamaica
+        'JP',  //- Japan
+        'JE',  //- Jersey
+        'JO',  //- Jordan
+        'KZ',  //- Kazakhstan
+        'KE',  //- Kenya
+        'KI',  //- Kiribati
+        'KW',  //- Kuwait
+        'KG',  //- Kyrgyzstan
+        'LA',  //- Laos
+        'LV',  //- Latvia
+        'LB',  //- Lebanon
+        'LS',  //- Lesotho
+        'LR',  //- Liberia
+        'LY',  //- Libya
+        'LI',  //- Liechtenstein
+        'LT',  //- Lithuania
+        'LU',  //- Luxembourg
+        'MO',  //- Macau
+        'MK',  //- Macedonia
+        'MG',  //- Madagascar
+        'MW',  //- Malawi
+        'MY',  //- Malaysia
+        'MV',  //- Maldives
+        'ML',  //- Mali
+        'MT',  //- Malta
+        'MH',  //- Marshall Islands
+        'MQ',  //- Martinique
+        'MR',  //- Mauritania
+        'MU',  //- Mauritius
+        'YT',  //- Mayotte
+        'MX',  //- Mexico
+        'MD',  //- Moldova
+        'MC',  //- Monaco
+        'MN',  //- Mongolia
+        'ME',  //- Montenegro
+        'MS',  //- Montserrat
+        'MA',  //- Morocco
+        'MZ',  //- Mozambique
+        'MM',  //- Myanmar
+        'NA',  //- Namibia
+        'NR',  //- Nauru
+        'NP',  //- Nepal
+        'NL',  //- Netherlands
+//        'AN',  //- Netherlands Antilles, not a Zen Cart country!
+        'NC',  //- New Caledonia
+        'NZ',  //- New Zealand
+        'NI',  //- Nicaragua
+        'NE',  //- Niger
+        'NG',  //- Nigeria
+        'NU',  //- Niue
+        'NF',  //- Norfolk Island
+        'KP',  //- North Korea
+        'MP',  //- Northern Mariana Islands
+        'NO',  //- Norway
+        'OM',  //- Oman
+        'PK',  //- Pakistan
+        'PW',  //- Palau
+        'PS',  //- Palestinian Territory Occupied
+        'PA',  //- Panama
+        'PG',  //- Papua New Guinea
+        'PY',  //- Paraguay
+        'PE',  //- Peru
+        'PH',  //- Philippines
+        'PN',  //- Pitcairn Islands
+        'PL',  //- Poland
+        'PT',  //- Portugal
+        'PR',  //- Puerto Rico
+        'QA',  //- Qatar
+        'RE',  //- Reunion
+        'RO',  //- Romania
+        'RU',  //- Russia
+        'RW',  //- Rwanda
+        'WS',  //- Samoa
+        'SM',  //- San Marino
+        'ST',  //- Sao Tome and Principe
+        'SA',  //- Saudi Arabia
+        'SN',  //- Senegal
+        'RS',  //- Serbia
+//        'CS',  //- Serbia and Montenegro, not a Zen Cart country!
+        'SC',  //- Seychelles
+        'SL',  //- Sierra Leone
+        'SG',  //- Singapore
+        'SK',  //- Slovakia
+        'SI',  //- Slovenia
+        'SB',  //- Solomon Islands
+        'SO',  //- Somalia
+        'ZA',  //- South Africa
+        'GS',  //- South Georgia and the South Sandwich Islands
+        'KR',  //- South Korea
+        'ES',  //- Spain
+        'LK',  //- Sri Lanka
+        'SH',  //- St Helena
+        'KN',  //- St Kitts and Nevis
+        'LC',  //- St Lucia
+        'PM',  //- St Pierre and Miquelon
+        'VC',  //- St Vincent and the Grenadines
+        'SD',  //- Sudan
+        'SR',  //- Suriname
+        'SJ',  //- Svalbard and Jan Mayen Islands
+        'SZ',  //- Swaziland
+        'SE',  //- Sweden
+        'CH',  //- Switzerland
+        'SY',  //- Syria
+        'TW',  //- Taiwan
+        'TJ',  //- Tajikistan
+        'TZ',  //- Tanzania
+        'TH',  //- Thailand
+        'TL',  //- Timor Leste
+        'TG',  //- Togo
+        'TK',  //- Tokelau
+        'TO',  //- Tonga
+        'TT',  //- Trinidad and Tobago
+        'TN',  //- Tunisia
+        'TR',  //- Turkey
+        'TM',  //- Turkmenistan
+        'TC',  //- Turks and Caicos Islands
+        'TV',  //- Tuvalu
+        'UG',  //- Uganda
+        'UA',  //- Ukraine
+        'AE',  //- United Arab Emirates
+        'GB',  //- United Kingdom
+        'US',  //- United States
+        'UY',  //- Uruguay
+        'UM',  //- US Minor Outlying Islands
+        'UZ',  //- Uzbekistan
+        'VU',  //- Vanuatu
+        'VA',  //- Vatican City State
+        'VE',  //- Venezuela
+        'VN',  //- Viet Nam
+        'VG',  //- Virgin Islands British
+        'VI',  //- Virgin Islands USA
+        'WF',  //- Wallis and Futuna Islands
+        'EH',  //- Western Sahara
+        'YE',  //- Yemen
+//        'YU',  //- Yugoslavia, not a Zen Cart country!
+        'ZM',  //- Zambia
+        'ZW',  //- Zimbabwe
     ];
 
     public static function convertCountryCode(string $country_code): string
