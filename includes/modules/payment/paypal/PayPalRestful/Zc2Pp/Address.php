c63a0642ce951aeb084750806fe98efc1dd21b3c
lat9 <lat9@vinosdefrutastropicales.com>
2024-09-04 18:48:24 -0400

    #44: Remove intervening spaces from addresses' postal_codes.

--- includes/modules/payment/paypal/PayPalRestful/Zc2Pp/Address.php
+++ includes/modules/payment/paypal/PayPalRestful/Zc2Pp/Address.php
@@ -5,6 +5,8 @@
  * @copyright Copyright 2023 Zen Cart Development Team
  * @license https://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: lat9 2023 Nov 16 Modified in v2.0.0 $
+ *
+ * Last updated: v1.0.5
  */
 namespace PayPalRestful\Zc2Pp;
 
@@ -18,7 +20,7 @@ class Address
             'address_line_1' => $order_address['street_address'],
             'admin_area_2' => $order_address['city'],
             'admin_area_1' => (!empty($order_address['state_code'])) ? $order_address['state_code'] : $order_address['state'],
-            'postal_code' => $order_address['postcode'],
+            'postal_code' => str_replace(' ', '', $order_address['postcode']),
             'country_code' => CountryCodes::convertCountryCode($order_address['country']['iso_code_2']),
         ];
         if (!empty($order_address['suburb'])) {
