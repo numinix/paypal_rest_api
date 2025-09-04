dda4a44357b2023c0756146e03f0b62ed2ac07fe
lat9 <lat9@vinosdefrutastropicales.com>
2025-09-04 08:41:10 -0400

    Update readmes/module version for v1.2.0 release

--- includes/modules/payment/paypalr.php
+++ includes/modules/payment/paypalr.php
@@ -33,7 +33,7 @@ use PayPalRestful\Zc2Pp\CreatePayPalOrderRequest;
  */
 class paypalr extends base
 {
-    protected const CURRENT_VERSION = '1.2.0-beta3';
+    protected const CURRENT_VERSION = '1.2.0';
 
     protected const REDIRECT_LISTENER = HTTP_SERVER . DIR_WS_CATALOG . 'ppr_listener.php';
 
