c2ba23df4edcf166093c458e4bcf742328de454f
Jeff Lew <jlew@numinix.com>
2025-11-25 10:13:49 -0800

    Add PayPal API endpoint handler

--- numinix.com/includes/extra_datafiles/numinix_paypal_isu.php
+++ numinix.com/includes/extra_datafiles/numinix_paypal_isu.php
@@ -5,3 +5,7 @@
 if (!defined('FILENAME_PAYPAL_SIGNUP')) {
     define('FILENAME_PAYPAL_SIGNUP', 'paypal_signup');
 }
+
+if (!defined('FILENAME_PAYPAL_API')) {
+    define('FILENAME_PAYPAL_API', 'paypal_api');
+}
