fe6cb269938255fbc45d0862250713ad658aa5e0
Jeff <jlew@numinix.com>
2025-11-27 12:47:45 -0800

    remove version update in 1.0.5 as it's already handled by the init

--- numinix.com/management/includes/installers/numinix_paypal_isu/1_0_5.php
+++ numinix.com/management/includes/installers/numinix_paypal_isu/1_0_5.php
@@ -5,9 +5,6 @@ if (!defined('IS_ADMIN_FLAG')) {
 
 global $db, $messageStack, $configuration_group_id;
 
-$targetVersion = '1.0.5';
-$versionKey = 'NUMINIX_PPCP_VERSION';
-
 $obsoleteKeys = [
     'NUMINIX_PPCP_BACKEND_URL' => 'Removed configurable proxy endpoint; onboarding now targets the hosted Numinix proxy automatically.',
     'NUMINIX_PPCP_PLUGIN_MODE' => 'Removed credential transfer mode; onboarding no longer persists API credentials to remote stores.',
@@ -31,33 +28,6 @@ foreach ($obsoleteKeys as $configKey => $notice) {
             $messageStack->add($notice, 'warning');
         }
     }
-}
-
-$versionEntry = $db->Execute(
-    "SELECT configuration_id, configuration_group_id"
-    . " FROM " . TABLE_CONFIGURATION
-    . " WHERE configuration_key = '" . zen_db_input($versionKey) . "'"
-    . " LIMIT 1"
-);
-
-if ($versionEntry && !$versionEntry->EOF) {
-    $db->Execute(
-        "UPDATE " . TABLE_CONFIGURATION
-        . " SET configuration_value = '" . zen_db_input($targetVersion) . "',"
-        . " last_modified = NOW()"
-        . " WHERE configuration_key = '" . zen_db_input($versionKey) . "'"
-        . " LIMIT 1"
-    );
-
-    if (!isset($configuration_group_id) || (int) $configuration_group_id <= 0) {
-        $configuration_group_id = (int) $versionEntry->fields['configuration_group_id'];
-    }
-} elseif (isset($configuration_group_id) && (int) $configuration_group_id > 0) {
-    $db->Execute(
-        "INSERT INTO " . TABLE_CONFIGURATION
-        . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added)"
-        . " VALUES ('Numinix PayPal ISU Version', '" . zen_db_input($versionKey) . "', '" . zen_db_input($targetVersion) . "', 'Installed version of the Numinix PayPal onboarding plugin.', " . (int) $configuration_group_id . ", 0, NOW())"
-    );
 }
 
 $configuration_group_id = (int) ($configuration_group_id ?? 0);
