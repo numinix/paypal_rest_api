4ab365824eb8719e7cb4d1dc0723f80be8c3d1f0
Jeff Lew <jlew@numinix.com>
2026-02-02 15:52:43 -0800

    Force expiration fields to full row width

--- includes/modules/payment/paypalr_creditcard.php
+++ includes/modules/payment/paypalr_creditcard.php
@@ -502,10 +502,10 @@ class paypalr_creditcard extends base
         $fields[] = [
             'title' => MODULE_PAYMENT_PAYPALR_CC_EXPIRES ?? 'Expiration Date',
             'field' =>
-                '<span class="ppr-cc-expiration">' .
+                '<div class="ppr-cc-expiration">' .
                 zen_draw_pull_down_menu('paypalr_cc_expires_month', $expires_month, date('m'), 'class="ppr-creditcard-field ppr-card-new" id="paypalr-cc-expires-month"' . $onFocus) .
                 zen_draw_pull_down_menu('paypalr_cc_expires_year', $expires_year, $this_year, 'class="ppr-creditcard-field ppr-card-new" id="paypalr-cc-expires-year"' . $onFocus) .
-                '</span>',
+                '</div>',
             'tag' => 'paypalr-cc-expires-month',
         ];
 
